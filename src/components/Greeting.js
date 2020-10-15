8558c6c27403cb8cedbb933d31dd285b944a3753
Takuma Kakehi <takuma.kakehi@gmail.com>
2020-10-15 13:25:59 -0400

    media was added to the header

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -5,8 +5,36 @@ const Greeting = () => {
 
     return (
         <div className="greeting-container">
-            <h1 className="v-padding-l">Nice to See You!</h1>
-            <p className="my-title v-padding-l">I'm Takuma, a Product and Interactive Designer.</p>
+            <div className="main-text">
+                <h1 className="v-padding-l">Nice to See You!</h1>
+                <p className="my-title v-padding-l">I'm Takuma, a Product and Interactive Designer.</p>
+            </div>
+            <div className="media-container">
+                <a className="media email" href="mailto:takuma.kakehi@gmail.com">
+                    <div>
+                        <img width="48" src="/.././svg/email.svg" />
+                    </div>
+                    <div>
+                        Contact me
+                    </div>
+                </a>
+                <a className="media dribbble" href="https://dribbble.com/tkakehi" target="_blank">
+                    <div>
+                        <img width="48" src="/.././svg/dribbble.svg" />
+                    </div>
+                    <div>
+                        Dribbble
+                    </div>
+                </a>
+                <a className="media medium" href="https://medium.com/@takuma.kakehi" target="_blank">
+                    <div>
+                        <img width="48" src="/.././svg/medium.svg" />
+                    </div>
+                    <div>
+                        Medium
+                    </div>
+                </a>
+            </div>
         </div>
     );
 
